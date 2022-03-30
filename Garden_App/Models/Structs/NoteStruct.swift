692de2d02347bc2003fa67309d7e832f8581ecee
Alina <zikibikk@gmail.com>
2022-03-30 19:43:04 +0300

    added some services and structs

---
+++ Garden_App/Models/Structs/NoteStruct.swift
@@ -0,0 +1,14 @@
+//
+//  NoteStruct.swift
+//  Garden_App
+//
+//  Created by Alina Bikkinina on 30.03.2022.
+//
+
+import Foundation
+struct NoteStruct {
+    let noteDate: Date
+    let noteText: String
+    let noteName: String?
+    let noteTags: Set<TagEntity>?
+}
---
+++ Garden_App/Models/Structs/TagStruct.swift
@@ -0,0 +1,14 @@
+//
+//  TagStruct.swift
+//  Garden_App
+//
+//  Created by Alina Bikkinina on 30.03.2022.
+//
+
+import Foundation
+struct TagStruct {
+    let tagColor: String?
+    let tagText: String
+    let tagNote: NoteEntity?
+    let tagPlant: PlantEntity?
+}
