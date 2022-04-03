62eaa3d1e6c13387413046d59067d2d93b17f406
Alina <zikibikk@gmail.com>
2022-04-03 17:26:15 +0300

    Revert "Revert "for copy""
    This reverts commit 03bfb34b0765afa2e6eeddab04d9f1250eebe265.

--- Garden_App/Services/ViewService.swift
+++ Garden_App/Services/ViewService.swift
@@ -5,51 +5,24 @@
 //  Created by Alina Bikkinina on 02.04.2022.
 //
 
-import UIKit
-import Foundation
+import SnapKit
 
-class ViewService {
-    
-    static func configureLabel(text: String, font: UIFont, color: UIColor) -> UILabel {
-        let resultLabel = UILabel()
-        resultLabel.text = text
-        resultLabel.font = font
-        resultLabel.textColor = color
-        return resultLabel
-    }
-    
-    //TODO: рамки по периметру текста, чтобы граница цета начиналась раньше, чем текст
-    static func configureNoteLabel(text: String, font: UIFont, color: UIColor, backgroundColor: UIColor) -> UILabel {
-        let resultLabel = UILabel()
-        resultLabel.text = text
-        resultLabel.font = font
-        resultLabel.textColor = color
-        
-        resultLabel.backgroundColor = backgroundColor
-        resultLabel.layer.masksToBounds = true
-        resultLabel.layer.cornerRadius = 15.0
-        resultLabel.numberOfLines = 0
-        return resultLabel
-    }
-    
-    static func configureTextView(text: String, font: UIFont, color: UIColor) -> UITextView {
-        let resultTextView = UITextView()
-        resultTextView.text = text
-        resultTextView.font = font
-        resultTextView.textColor = color
-        return resultTextView
-    }
-    
-}
-
-class AdviceView: UIView {
-    private lazy var titleLabel: UILabel = {
+class DeepGreenView: UIView {
+    private lazy var label: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 14)
+        label.numberOfLines = 0
+        label.font = .title
+        label.textColor = .white
         return label
     }()
 
-    private lazy var backgroundView: UIView = UIView()
+    private lazy var backgroundView: UIView = {
+        let view = UIView()
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 15.0
+        view.backgroundColor = .deepGreen
+        return view
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -62,21 +35,37 @@ class AdviceView: UIView {
 
     private func setup() {
         addSubview(backgroundView)
-        backgroundView.snp.makeConstraints {
+        /*backgroundView.snp.makeConstraints {
+            $0.edges.equalTo(label)
+        }*/
+        
+        addSubview(label)
+        /*label.snp.makeConstraints {
             $0.edges.equalToSuperview()
-        }
-
-        addSubview(titleLabel)
+        }*/
     }
 
     func configure(text: String) {
-        titleLabel.text = text
+        label.text = text
     }
 
     func configure(font: UIFont) {
-        titleLabel.font = font
+        label.font = font
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 protocol ICalculationService {
